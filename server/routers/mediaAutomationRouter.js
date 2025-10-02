689cb754417a86aef663a4ec4ad48305c3e91f32
Ikra Nur Kurtoglu <kurtogluikranur@gmail.com>
2025-10-02 13:43:49 +0300

    server-side validation added.4

--- server/routers/mediaAutomationRouter.js
+++ server/routers/mediaAutomationRouter.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
-
 const media = require('../data/data-model');
+const { body, validationResult } = require('express-validator');
+
+const errorValid = () => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+}
 
 router.get("/", async (req, res, next) => {
     try {
@@ -35,7 +42,22 @@ router.get("/:id", async (req, res, next) => {
 })
 
 
-router.post("/", async (req, res, next) => {
+router.post("/", [
+    body("name")
+        .trim()
+        .notEmpty()
+        .withMessage("Sosyal medya ismi zorunludur."),
+    body("link")
+        .trim()
+        .notEmpty()
+        .withMessage("Sosyal medya linki zorunludur.")
+        .isURL()
+        .withMessage("Geçerli bir URL giriniz.")
+
+], async (req, res, next) => {
+
+    errorValid();
+
     try {
         const newMedia = req.body;
         console.log("POST ile gelen veri:", newMedia);
@@ -46,6 +68,7 @@ router.post("/", async (req, res, next) => {
             console.log(result);
             return res.status(201).json(result);
         } else {
+
             next({
                 statusCode: 400,
                 errorMessage: "Sosyal medya eklemek icin isim ve link girmelisiniz.",
@@ -58,23 +81,40 @@ router.post("/", async (req, res, next) => {
 });
 
 
-router.patch("/:id", async (req, res, next) => {
-    try {
-        const id = parseInt(req.params.id);
-        const updateMedia = req.body;
+router.patch("/:id",
+    [
+        body("name")
+            .optional()
+            .trim()
+            .notEmpty()
+            .withMessage("Sosyal medya ismi boş olamaz."),
+        body("link")
+            .optional()
+            .trim()
+            .notEmpty()
+            .withMessage("Sosyal medya linki boş olamaz.")
+            .isURL()
+            .withMessage("Geçerli bir URL giriniz.")
+    ], async (req, res, next) => {
 
-        if (updateMedia) {
-            const updated = await media.updatedMedia(updateMedia, id);
-            return res.status(200).json(updated);
+        errorValid();
+
+        try {
+            const id = parseInt(req.params.id);
+            const updateMedia = req.body;
+
+            if (updateMedia) {
+                const updated = await media.updatedMedia(updateMedia, id);
+                return res.status(200).json(updated);
+            }
+        } catch (error) {
+            next({
+                statusCode: 500,
+                errorMessage: "Sosyal medya duzenlenirken hata olustu.",
+                error,
+            });
         }
-    } catch (error) {
-        next({
-            statusCode: 500,
-            errorMessage: "Sosyal medya duzenlenirken hata olustu.",
-            error,
-        });
-    }
-})
+    })
 
 
 router.delete("/:id", async (req, res, next) => {
